a59df83747f0d4fe5acaaade770aebffdb13db84
Stridemann <stridemann@gmail.com>
2018-09-27 21:58:33 +0300

    Initial commit

---
+++ src/DelveHelpers/Core.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using SharpDX;
+using SharpDX.Direct3D9;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using PoeHUD.Hud.Menu;
+using PoeHUD.Hud.Settings;
+using PoeHUD.Models.Enums;
+using PoeHUD.Plugins;
+using PoeHUD.Poe;
+using PoeHUD.Models;
+using PoeHUD.Poe.Components;
+using PoeHUD.Poe.Elements;
+using PoeHUD.Poe.EntityComponents;
+using PoeHUD.Poe.RemoteMemoryObjects;
+
+namespace DelveHelpers
+{
+	public class Core : BaseSettingsPlugin<Settings>
+	{
+		public Core()
+		{
+			PluginName = "DelveHelpers";
+		}
+
+		public override void Initialise()
+		{
+			//Called on start
+		}
+
+		private RectangleF DrawRect;
+		//
+		public override void Render()
+		{
+			DrawRect = new RectangleF(Settings.PosX, Settings.PosY, 70, 30);
+
+			var playerStats = GameController.Player.GetComponent<Stats>().StatDictionary;
+
+			var sc = playerStats[GameStat.DelveSulphiteCapacity];
+
+			DrawData("Resources/Sulphite.png", GameController.Game.IngameState.ServerData.CurrentSulphiteAmount + "/" + sc);
+			DrawData("Resources/Azurite.png", GameController.Game.IngameState.ServerData.CurrentAzuriteAmount.ToString());
+
+
+			var buff = GameController.Player.GetComponent<Life>().Buffs.FirstOrDefault(x => x.Name == "delve_degen_buff");
+			//if(buff != null)
+			DrawData("Resources/Buff.png", buff == null ? "-" : buff.Charges.ToString());
+		}
+
+		private void DrawData(string icon, string data)
+		{
+			var textSize = Graphics.MeasureText(data, 20, FontDrawFlags.Left | FontDrawFlags.VerticalCenter);
+
+			DrawRect.Width = DrawRect.Height + textSize.Width + 10;
+
+			Graphics.DrawBox(DrawRect, Color.Black);
+
+			var imgRect = DrawRect;
+			imgRect.Width = imgRect.Height;
+			Graphics.DrawPluginImage(Path.Combine(PluginDirectory, icon), imgRect);
+			Graphics.DrawFrame(DrawRect, 2, Color.Gray);
+
+			var textPos = DrawRect.TopLeft;
+			textPos.Y += DrawRect.Height / 2;
+			textPos.X += DrawRect.Height + 3;
+
+			Graphics.DrawText(data, 20, textPos, FontDrawFlags.Left | FontDrawFlags.VerticalCenter);
+
+		
+
+			DrawRect.X += DrawRect.Width + 1;
+		}
+	}
+}
---
+++ src/DelveHelpers/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+using SharpDX;
+using PoeHUD.Plugins;
+using PoeHUD.Hud.Settings;
+
+namespace DelveHelpers
+{
+	//All properties and public fields of this class will be saved to file
+	public class Settings : SettingsBase
+	{
+		public Settings()
+		{
+			Enable = true;
+		}
+
+		[Menu("PosX")]
+		public RangeNode<int> PosX { get; set; } = new RangeNode<int>(551, 0, 1500);
+
+		[Menu("PosY")]
+		public RangeNode<int> PosY { get; set; } = new RangeNode<int>(0, 0, 1500);
+
+		[Menu("Width")]
+		public RangeNode<int> Width { get; set; } = new RangeNode<int>(0, 0, 100);
+	}
+}
